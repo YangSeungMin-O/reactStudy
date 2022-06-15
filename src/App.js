b9a733299fe5d6cdbf26b8bcac281649e3e5f022
YangSeungMin <ysm6768@sodasys.com>
2022-06-15 10:51:07 +0900

    리액트 틱택토 게임

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import TicTacToe from './TicTacToc/TicTacToe';
+
+function App() {
+  return (
+    <div className="App">
+        <div>
+          <TicTacToe />
+        </div>
+    </div>
+  );
+}
+
+export default App;
